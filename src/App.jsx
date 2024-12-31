aeb4ec275e2d71692e19cea5b6eca3ef18eea20e
Naimor Rahaman Durjoy <Durjoy@Userui-Macmini.local>
2024-12-31 17:34:09 +0600

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+   <div>
+     {/* <DaisyNav></DaisyNav> */}
+     <NavBar></NavBar>
+     
+   </div>
+  )
+}
+
+export default App
